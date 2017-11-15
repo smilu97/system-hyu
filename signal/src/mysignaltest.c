f565371c7494109114c5c2a3189ac76dd0c4e87d
smilu97 <smilup2244@gmail.com>
2017-11-15 21:15:21 +0900

    signal 3 complete

--- signal/src/mysignaltest.c
+++ signal/src/mysignaltest.c
@@ -6,83 +6,73 @@
 
 static jmp_buf env_alrm;
 
+int doing_sig_int = 0;
+
 static void sig_alrm(int signo);
 static void sig_int(int signo);
 unsigned int sleep2(unsigned int seconds);
-static void sig_quit(int signo);
+
+void nothing() {}
 
 int main(void)
 {
-   unsigned int unslept;
-   sigset_t newmask,oldmask;
+	unsigned int unslept;
 
-   if(signal(SIGINT, sig_int) == SIG_ERR){
-      fprintf(stderr, "signal(SIGINT) error");
-      exit(-1);
-   }
-   if(signal(SIGQUIT,sig_quit) == SIG_ERR){
-      fprintf(stderr, "signal(SIGQUIT) error");
-      exit(-1);
-   }
+	if (signal(SIGINT, sig_int) == SIG_ERR){
+		fprintf(stderr, "signal(SIGINT) error");
+		exit(-1);
+	}
+	if (signal(SIGUSR1, nothing) == SIG_ERR) {
+		fprintf(stderr, "signal(SIGUSR1) error");
+		exit(-1);
+	}
+	
+	unslept = sleep2(5);
 
-   sigemptyset(&newmask);
-   sigaddset(&newmask,SIGQUIT);
-   if(sigprocmask(SIG_BLOCK,&newmask,&oldmask) < 0) {
-	  fprintf(stderr, "SIG_BLOCK error");
-   }
+	printf("sleep2 returned: %u\n", unslept);
 
-   unslept = sleep2(5);
-   
-   printf("sleep2 returned: %u\n", unslept);
-
-   //restore signal mask 
-   if(sigprocmask(SIG_SETMASK, &oldmask, NULL) < 0) {
-	  fprintf(stderr, "SIG_SETMASK error");
-   }
-   printf("SIGQUIT unblocked\n");
-
-   return 0;
-}
-
-static void sig_quit(int signo){
-   printf("caught SiGQUIT\n");
-   if(signal(SIGQUIT,SIG_DFL)==SIG_ERR)
-      printf("can't reset SIGQUIT\n");
+	return 0;
 }
 
 static void sig_alrm(int signo)
 {
-   longjmp(env_alrm, 1);
+	if(doing_sig_int) return;
+	longjmp(env_alrm, 1);
 }
 
-/*
- *this has problem abort other signal even if it's not finished
- */
 static void sig_int(int signo)
 {
-   int i,j;
-   volatile int k;
+	if(doing_sig_int) return; 
 
-   printf("\nsig_int starting\n");
+	int i,j;
+	volatile int k;
 
-   /*
-    * 아래 for문이 5초 이상 실행되도록 적당히 바꿔주세요. 
-    */
-   for (i = 0; i<300000; i++)
-      for (j = 0; j<4000; j++)
-         k += i*j;
-   
-   printf("sig_int finished\n");
+	printf("\nsig_int starting\n");
+	doing_sig_int = 1;
+	
+	/*
+	 * 아래 for문이 5초 이상 실행되도록 적당히 바꿔주세요. 
+	 */
+	for (i = 0; i<100; i++) {
+		for (j = 0; j<12000000; j++) {
+			k += i*j;	
+		}
+		printf("sigin: %d\n", i);
+	}
+		
+	
+	printf("sig_int finished\n");
+	doing_sig_int = 0;
 }
 
 unsigned int sleep2(unsigned int seconds)
 {
-   if (signal(SIGALRM, sig_alrm) == SIG_ERR)
-      return seconds;
+	if (signal(SIGALRM, sig_alrm) == SIG_ERR)
+		return seconds;
 
-   if (setjmp(env_alrm) == 0){
-      alarm(seconds);
-      pause();
-   }
-   return alarm(0);
+	if (setjmp(env_alrm) == 0){
+		alarm(seconds);
+		pause();
+	}
+	return alarm(0);
 }
